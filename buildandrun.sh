f1deadeefda2153b4790bbde08f8817f2dde735f
00008966 <asanka.gunatilaka@medavie.bluecross.ca>
2025-04-05 10:31:04 -0400

    Fix for db connection issue - allowpublickey

--- buildandrun.sh
+++ buildandrun.sh
@@ -1,3 +1,5 @@
+export JAVA_HOME="/c/development/IBM/jdk-17.0.9"
+export PATH="$JAVA_HOME/bin:$PATH"
 mvn clean install
 docker build -t homefinance:1.0 .
 docker run --rm --detach --name=homefinance -p 8585:8585 homefinance:1.0
