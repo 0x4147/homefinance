bb398bc96c25b39840c08baacbc222907758d7dd
00008966 <asanka.gunatilaka@medavie.bluecross.ca>
2025-05-19 10:50:06 -0400

    Flyway implmentation

---
+++ src/main/resources/db/migration/V2__initial_data.sql
@@ -0,0 +1,25 @@
+INSERT INTO Person (name, email)
+VALUES
+    ('Asanka', 'asanka.nz@gmail.com'),
+    ('Divya', 'divyamehta.nz@gmail.com');
+
+INSERT INTO Category (name, type)
+VALUES
+    ('Utility Bill', 'EXPENSE'),
+    ('Groceries', 'EXPENSE'),
+    ('Transportation', 'EXPENSE'),
+    ('Healthcare', 'EXPENSE'),
+    ('Entertainment', 'EXPENSE'),
+    ('Mortgage', 'EXPENSE'),
+    ('Property tax', 'EXPENSE'),
+    ('Insurance', 'EXPENSE'),
+    ('Car repair', 'EXPENSE'),
+    ('Home maintenance', 'EXPENSE'),
+    ('Salary', 'INCOME'),
+    ('Rental Income', 'INCOME'),
+    ('Government Grants', 'INCOME'),
+    ('Take Out', 'INCOME'),
+    ('Amazon', 'INCOME'),
+    ('Temu', 'INCOME');
+
+
